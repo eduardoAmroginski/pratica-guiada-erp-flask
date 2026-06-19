bdfeacb6f9d0065bf24405189a96efac021cf661
Eduardo Amroginski Santos <eduardo-amroginski@hotmail.com>
2026-06-18 21:37:29 -0300

    WIP: produto/form.html

--- app/routes/web_bp.py
+++ app/routes/web_bp.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 
-from app.controllers import (produtos_controller)
+from app.controllers import (produtos_controller, categoria_controller)
 
 web_bp = Blueprint("web", __name__)
 
@@ -13,3 +13,20 @@ def index():
 def listar_produtos_view():
     produtos = produtos_controller.listar_todos_produtos()
     return render_template("produtos/listar.html", produtos=produtos)
+
+@web_bp.route("/produto/novo", methods=["GET", "POST"])
+def novo_produto_view():
+    categorias = categoria_controller.listar_todas_categorias()
+
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        preco = float(request.form.get("preco", 0))
+        categoria_id = int(request.form.get("categoria_id", 0))
+
+        sucesso, msg = produtos_controller.salvar_produto(nome, preco, categoria_id)
+        flash(msg, "success" if sucesso else "danger")
+
+        if sucesso:
+            return redirect(url_for("web.listar_produtos_view"))
+        
+    return render_template("produtos/form.html", produto=None, categorias=categorias)
